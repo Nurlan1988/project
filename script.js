9ce036eaa4aac95434038dfb227dba57622bff7e
Nurlan <example@gmail.com>
2021-03-13 19:19:13 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+
+
+//const obj = {
+   //name: "John",
+   //age: 25,
+  // isMarried: false
+//};
+//console.log (obj["name"]);
+
+
+//let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+//console.log(arr[1]);
+
+
+//alert('Hello');
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = +prompt("Вам есть 18?", "18");
+//console.log(typeof(answer));
+
+//const answers = [];
+
+//answers [0] = prompt('Как ваше имя?', '');
+//answers [1] = prompt('Как ваше фамилия?', '');
+//answers [2] = prompt('Сколько вам лет?', '');
+//console.log(answers);
+
+//const category = 'toys';
+//console.log(`https://someurl.com/${category}/5`);
+
+//const user = "Ivan";
+//alert(`Привет, ${user}`);
+
+
+//console.log('arr' + "- object");
+//console.log(4 + "- object");
+
+//let incr = 10,
+    //decr = 10;
+//incr++;
+//decr--;
+//console.log(++incr);
+//console.log(--decr);
+
+//console.log(5%2);
+
+//console.log(2*4==='8');
+
+//const isChecked = false;
+      //isClose = false;
+//console.log(isChecked || !isClose ); 
